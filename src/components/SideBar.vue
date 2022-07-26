c07f0883bd1b20feda13746ea51971f63cdb47ba
Sofiene mokaddem <73641797+sofieneMoka@users.noreply.github.com>
2022-07-26 12:25:16 +0100

    add sidebar in home page

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,235 +1,256 @@
 <template>
-    <div class="area"></div><nav class="main-menu">
-            <ul>
-                <li>
-                    <a href="http://justinfarrow.com">
-                        <i class="fa fa-home fa-2x"></i>
-                        <span class="nav-text">
-                            Dashboard
-                        </span>
-                    </a>
-                  
-                </li>
-                <li class="has-subnav">
-                    <a href="#">
-                        <i class="fa fa-laptop fa-2x"></i>
-                        <span class="nav-text">
-                            Stars Components
-                        </span>
-                    </a>
-                    
-                </li>
-                <li class="has-subnav">
-                    <a href="#">
-                       <i class="fa fa-list fa-2x"></i>
-                        <span class="nav-text">
-                            Forms
-                        </span>
-                    </a>
-                    
-                </li>
-                <li class="has-subnav">
-                    <a href="#">
-                       <i class="fa fa-folder-open fa-2x"></i>
-                        <span class="nav-text">
-                            Pages
-                        </span>
-                    </a>
-                   
-                </li>
-                <li>
-                    <a href="#">
-                        <i class="fa fa-bar-chart-o fa-2x"></i>
-                        <span class="nav-text">
-                            Graphs and Statistics
-                        </span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <i class="fa fa-font fa-2x"></i>
-                        <span class="nav-text">
-                           Quotes
-                        </span>
-                    </a>
-                </li>
-                <li>
-                   <a href="#">
-                       <i class="fa fa-table fa-2x"></i>
-                        <span class="nav-text">
-                            Tables
-                        </span>
-                    </a>
-                </li>
-                <li>
-                   <a href="#">
-                        <i class="fa fa-map-marker fa-2x"></i>
-                        <span class="nav-text">
-                            Maps
-                        </span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                       <i class="fa fa-info fa-2x"></i>
-                        <span class="nav-text">
-                            Documentation
-                        </span>
-                    </a>
-                </li>
-            </ul>
+  <div
+    class="sidebar open"
+    :style="cssVars"
+  >
 
-            <ul class="logout">
+    <div style="display: flex ; flex-direction:column; justify-content: space-between; flex-grow: 1; max-height: calc(100% - 80px); ">
+      <div
+        id="my-scroll"
+        style="margin: 6px 14px 0 14px;"
+      >
+        <ul class="nav-list">
+          <li>
+            <i class="bx bx-search" />
+            <input
+              type="text"
+              placeholder="Search..."
+            >
+          </li>
+
+          <span
+            v-for="(menuItem, index) in menuItems"
+            :key="index"
+          >
+            <li v-on:click="seen = !seen">
+              <a :href="menuItem.link">
+                <i class="bx bx-folder" />
+                <span class="Category_name">{{ menuItem.name }}</span>
+              </a>
+            </li>
+            <div v-if="seen">
                 <li>
-                   <a href="#">
-                         <i class="fa fa-power-off fa-2x"></i>
-                        <span class="nav-text">
-                            Logout
-                        </span>
-                    </a>
-                </li>  
-            </ul>
-        </nav>
+                    <span class="Subcategory_name">aaaa{{ menuItem.name }}</span>
+                </li>
+                <li>
+                    <span class="Subcategory_name">bbbbb</span>
+                </li>
+            </div>
+          </span>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-@import url(//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css);
-@import url(https://fonts.googleapis.com/css?family=Titillium+Web:300);
-.fa-2x {
-font-size: 2em;
-}
-.fa {
-position: relative;
-display: table-cell;
-width: 60px;
-height: 36px;
-text-align: center;
-vertical-align: middle;
-font-size:20px;
-}
-
-
-.main-menu:hover,nav.main-menu.expanded {
-width:250px;
-overflow:visible;
-}
-
-.main-menu {
-background:#212121;
-border-right:1px solid #e5e5e5;
-position:absolute;
-height:100%;
-left:0;
-width:60px;
-overflow:hidden;
--webkit-transition:width .05s linear;
-transition:width .05s linear;
--webkit-transform:translateZ(0) scale(1,1);
-z-index:1000;
-}
-
-.main-menu>ul {
-margin:7px 0;
-}
-
-.main-menu li {
-position:relative;
-display:block;
-width:250px;
-}
-
-.main-menu li>a {
-position:relative;
-display:table;
-border-collapse:collapse;
-border-spacing:0;
-color:#999;
- font-family: arial;
-font-size: 14px;
-text-decoration:none;
--webkit-transform:translateZ(0) scale(1,1);
--webkit-transition:all .1s linear;
-transition:all .1s linear;
-  
-}
-
-.main-menu .nav-icon {
-position:relative;
-display:table-cell;
-width:60px;
-height:36px;
-text-align:center;
-vertical-align:middle;
-font-size:18px;
-}
-
-.main-menu .nav-text {
-position:relative;
-display:table-cell;
-vertical-align:middle;
-width:190px;
-  font-family: 'Titillium Web', sans-serif;
-}
-
-.main-menu>ul.logout {
-position:absolute;
-left:0;
-bottom:0;
-}
-
-.no-touch .scrollable.hover {
-overflow-y:hidden;
-}
-
-.no-touch .scrollable.hover:hover {
-overflow-y:auto;
-overflow:visible;
-}
-
-a:hover,a:focus {
-text-decoration:none;
-}
-
-nav {
--webkit-user-select:none;
--moz-user-select:none;
--ms-user-select:none;
--o-user-select:none;
-user-select:none;
-}
-
-nav ul,nav li {
-outline:0;
-margin:0;
-padding:0;
-}
-.main-menu li:hover>a,nav.main-menu li.active>a,.dropdown-menu>li>a:hover,.dropdown-menu>li>a:focus,.dropdown-menu>.active>a,.dropdown-menu>.active>a:hover,.dropdown-menu>.active>a:focus,.no-touch .dashboard-page nav.dashboard-menu ul li:hover a,.dashboard-page nav.dashboard-menu ul li.active a {
-color:#fff;
-background-color:#5fa2db;
-}
-.area {
-float: left;
-background: #e2e2e2;
-width: 100%;
-height: 100%;
-}
-@font-face {
-  font-family: 'Titillium Web';
-  font-style: normal;
-  font-weight: 300;
-  src: local('Titillium WebLight'), local('TitilliumWeb-Light'), url(http://themes.googleusercontent.com/static/fonts/titilliumweb/v2/anMUvcNT0H1YN4FII8wpr24bNCNEoFTpS2BTjF6FB5E.woff) format('woff');
-}
-
-</style>
-
-
-
-
-
 <script>
-export default {
-  name: 'SideBar'
-}
+  export default {
+    name: 'SideBar',
+    data(){
+        return{
+            seen:false
+        }
+    },
+    props: {
+
+      //! Menu items
+      menuItems: {
+        type: Array,
+        default: () => [
+          {
+            link: '#',
+            name: 'Dashboard',
+            tooltip: 'Dashboard',
+            icon: 'bx-grid-alt',
+          },
+          {
+            link: '#',
+            name: 'User',
+            tooltip: 'User',
+            icon: 'bx-user',
+          },
+          {
+            link: '#',
+            name: 'Messages',
+            tooltip: 'Messages',
+            icon: 'bx-chat',
+          },
+          {
+            link: '#',
+            name: 'Analytics',
+            tooltip: 'Analytics',
+            icon: 'bx-pie-chart-alt-2',
+          },
+          {
+            link: '#',
+            name: 'File Manager',
+            tooltip: 'Files',
+            icon: 'bx-folder',
+          },
+          {
+            link: '#',
+            name: 'Order',
+            tooltip: 'Order',
+            icon: 'bx-cart-alt',
+          },
+          {
+            link: '#',
+            name: 'Saved',
+            tooltip: 'Saved',
+            icon: 'bx-heart',
+          },
+          {
+            link: '#',
+            name: 'Setting',
+            tooltip: 'Setting',
+            icon: 'bx-cog',
+          },
+          {
+            link: '#',
+            name: 'Setting',
+            tooltip: 'Setting',
+            icon: 'bx-cog',
+          },
+          {
+            link: '#',
+            name: 'Setting',
+            tooltip: 'Setting',
+            icon: 'bx-cog',
+          },
+        ]
+      }
+    },
+    methods:{
+            toggleDropdown (event) {
+                event.currentTarget.classList.toggle('is-active')
+            }
+        }
+  }
 </script>
+
+<style>
+  /* Google Font Link */
+  @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap');
+  @import url('https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css');
+  * {
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Poppins', sans-serif;
+  }
+  .Subcategory_name{
+    flex-direction: column;
+  }
+  body {
+    transition: all 0.5s ease;
+  }
+  .sidebar {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    position: fixed;
+    left: 0;
+    margin-left: 0.5rem;
+    height: 100%;
+    min-height: min-content;
+    width: 250px;
+    /* overflow-y: auto; */
+    background-color: rgba(230, 193, 134, 0.4);
+    /* padding: 6px 14px 0 14px; */
+    z-index: 99;
+    border-radius: 0.5rem;
+  }
+  .sidebar i {
+    color: white;
+    height: 60px;
+    min-width: 50px;
+    font-size: 28px;
+    text-align: center;
+    line-height: 60px;
+  }
+  .sidebar .nav-list {
+    margin-top: 20px;
+  }
+  .sidebar li {
+    position: relative;
+    margin: 8px 0;
+    list-style: none;
+  }
+  .sidebar li .tooltip {
+    position: absolute;
+    top: -20px;
+    left: calc(100% + 15px);
+    z-index: 3;
+    box-shadow: 0 5px 10px rgba(0, 0, 0, 0.3);
+    padding: 6px 12px;
+    border-radius: 4px;
+    font-size: 15px;
+    font-weight: 400;
+    opacity: 0;
+    white-space: nowrap;
+    pointer-events: none;
+    transition: 0s;
+  }
+  .sidebar input {
+    font-size: 15px;
+    color: rgb(255, 255, 255);
+    font-weight: 400;
+    outline: none;
+    height: 50px;
+    padding: 0 20px 0 50px;
+    width: 100%;
+    border: none;
+    border-radius: 12px;
+    transition: all 0.5s ease;
+    background-color: rgb(136, 110, 68);
+  }
+  .sidebar .bx-search {
+    position: absolute;
+    top: 50%;
+    left: 0;
+    transform: translateY(-50%);
+    font-size: 22px;
+  }
+  .sidebar li a {
+    display: flex;
+    height: 100%;
+    width: 100%;
+    border-radius: 12px;
+    align-items: center;
+    text-decoration: none;
+    transition: all 0.4s ease;
+    background-color: rgba(230, 193, 134, 0.4);
+  }
+  .sidebar li a:hover {
+    background: rgb(240, 148, 10,0.4);
+    padding-bottom: 50px;
+  }
+  .sidebar li a .Category_name {
+    color: white;
+    font-size: 15px;
+    font-weight: 600;
+    white-space: nowrap;
+    opacity: 1;
+    pointer-events: auto;
+    transition: 0.4s;
+  }
+  .sidebar li a:hover .Category_name,
+  .sidebar li a:hover i {
+    transition: all 0.5s ease;
+    color: black;
+  }
+  .sidebar li i {
+    height: 50px;
+    line-height: 50px;
+    font-size: 18px;
+    border-radius: 12px;
+  }
+  .my-scroll-active {
+    overflow-y: auto;
+  }
+  #my-scroll {
+    overflow-y: auto;
+  }
+  #my-scroll::-webkit-scrollbar{
+    display:none;
+  }
+</style>
