54c90976f8f320bdd42db0dd591e058f9a9bdd37
Sofiene mokaddem <73641797+sofieneMoka@users.noreply.github.com>
2022-08-05 15:42:32 +0100

    add pdf viewer

---
+++ src/components/HomeComponent/ViewDocument.vue
@@ -0,0 +1,77 @@
+<template>
+  <div id="app">
+    <!-- <label for="file-upload" class="custom-file-upload">
+    Open PDF
+    </label>
+    <input id="file-upload" type="file" @change="openDocument" class="btn" /> -->
+    <PSPDFKitContainer :pdfFile="pdfFile" @loaded="handleLoaded"/>
+  </div>
+</template>
+
+<script>
+import PSPDFKitContainer from "@/components/HomeComponent/PSPDFKitContainer.vue";
+
+export default {
+  name : 'ViewDocument',
+  data() {
+    return {
+      pdfFile: this.pdfFile || "../../../../../lettre de motivation .pdf",
+    };
+  },
+  /**
+   * Render the `PSPDFKitContainer` component.
+   */
+  components: {
+    PSPDFKitContainer,
+  },
+  /**
+   * Our component has two methods — one to check when the document is loaded, and the other to open the document.
+   */
+  methods: {
+    handleLoaded(instance) {
+      console.log("PSPDFKit has loaded: ", instance);
+      // Do something.
+    },
+
+    // openDocument(event) {
+    //   // To access the Vue.js instance data properties, use `this` keyword.
+    //   if (this.pdfFile && this.pdfFile.startsWith('blob:')) {
+    //     window.URL.revokeObjectURL(this.pdfFile);
+    //   }
+    //   this.pdfFile = window.URL.createObjectURL(event.target.files[0]);
+    //   console.log(this.pdfFile)
+    // },
+  },
+};
+</script>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+}
+
+body {
+  margin: 0;
+}
+
+input[type="file"] {
+    display: none;
+}
+
+.custom-file-upload {
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    display: inline-block;
+    padding: 6px 12px;
+    cursor: pointer;
+    background:#4A8FED;
+    padding:10px;
+    color:#fff;
+    font:inherit;
+    font-size: 16px;
+    font-weight: bold;
+}
+
+</style>
